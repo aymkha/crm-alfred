f5aa6daebc46a0ff128a976ce05e523aae414c07
Ayman <ayman.khattar.ing@gmail.com>
2025-12-28 13:19:44 +0100

    FIX import restaurants

--- public/legacy/config_override.php
+++ public/legacy/config_override.php
@@ -4,4 +4,7 @@ $sugar_config['default_export_charset'] = 'ISO-8859-1';
 $sugar_config['system_name'] = 'Alfred CRM';
 $sugar_config['disabled_languages'] = '';
 $sugar_config['max_dashlets_homepage'] = '20';
+$sugar_config['logger']['level'] = 'debug';
+$sugar_config['logger']['file']['name'] = 'suitecrm';
+$sugar_config['logger']['file']['ext'] = '.log';
 /***CONFIGURATOR***/
